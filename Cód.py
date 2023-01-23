143680db9af2163b5f3609de45cb483fa1dfca79
Matheus Henrique Da Silva <matheush5529@gmail.com>
2023-01-23 20:35:57 -0300

    visualização
    Deixando o código mais simples de se entender

--- Cód.py
+++ Cód.py
@@ -5,14 +5,19 @@ print('****************************')
 numerosecreto = 42
 
 chute = int(input('Digíte seu número: '))
+
+acertou = chute == numerosecreto
+maior = chute > numerosecreto
+menor = chute < numerosecreto
+
 print('Você digítou o número:', chute)
 
-if(numerosecreto == chute):
+if(acertou):
     print('você acertou')
 else:
-    if(chute > numerosecreto):
+    if(maior):
         print('você errou! O seu chute foi maior do que o número secreto.')
-    elif(chute < numerosecreto):
+    elif(menor):
         print('você errou! O chute foi menor do que o número secreto.')
 
 print('Fim do Jogo')
