121b6e059c919817c2b85ce147e1cbc3716f4ace
Matheus Henrique Da Silva <matheush5529@gmail.com>
2023-01-23 22:18:53 -0300

    interação
    Resolvendo problemas de interação com o Usuário

--- Cód.py
+++ Cód.py
@@ -8,7 +8,13 @@ rodada = 1
 
 for rodada in range (1, total_de_tentativas + 1):
     print('Tentativa {} de {}.'.format(rodada, total_de_tentativas))
-    chute = int(input('Digíte seu número: '))
+    chute = int(input('Digíte um número entre 1 e 100: '))
+
+    if (chute < 1 or chute > 100):
+        print('Você deve digitar um número entre 1 e 100!')
+        continue
+
+
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
     menor = chute < numero_secreto
