6adb1670967d2697a6c30127f8531f8e74f56845
Matheus Henrique Da Silva <matheush5529@gmail.com>
2023-01-23 22:03:54 -0300

    estrutura de repetição for
    aplicando a estrutura de repetição for. para simplificar  a visualização do  código.

--- Cód.py
+++ Cód.py
@@ -6,7 +6,7 @@ numero_secreto = 42
 total_de_tentativas = 3
 rodada = 1
 
-while(rodada <= total_de_tentativas):
+for rodada in range (1, total_de_tentativas + 1):
     print('Tentativa {} de {}.'.format(rodada, total_de_tentativas))
     chute = int(input('Digíte seu número: '))
     acertou = chute == numero_secreto
@@ -22,6 +22,5 @@ while(rodada <= total_de_tentativas):
             print('você errou! O seu chute foi maior do que o número secreto.')
         elif(menor):
             print('você errou! O chute foi menor do que o número secreto.')
-    rodada = rodada + 1
 
     print('Fim do Jogo')
