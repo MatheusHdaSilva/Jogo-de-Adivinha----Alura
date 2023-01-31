2559b9a9710e40fbf655aa6bc0c8a5803e271f27
Matheus Henrique Da Silva <matheush5529@gmail.com>
2023-01-30 23:01:54 -0300

    Dificuldade
    Adicionando dificuldade ao jogo, o usuário será capaz de escolher um dos níveis disponíveis. E o número de tentativas mudara de acordo com o Nível escolhido

--- Cód.py
+++ Cód.py
@@ -6,13 +6,24 @@ print('Jogo de adivinhação!')
 print('****************************')
 
 numero_secreto = random.randrange(1, 101)
-total_de_tentativas = 3
+total_de_tentativas = 0
 rodada = 1
+
+print('Qual Nível de dificuldade?')
+print('(1) Facíl (2) Médio (3) Difícil')
+
+nivel = int(input('Defina um Nível: '))
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range (1, total_de_tentativas + 1):
     print('Tentativa {} de {}.'.format(rodada, total_de_tentativas))
+    
     chute = int(input('Digíte um número entre 1 e 100: '))
-
     if (chute < 1 or chute > 100):
         print('Você deve digitar um número entre 1 e 100!')
         continue
