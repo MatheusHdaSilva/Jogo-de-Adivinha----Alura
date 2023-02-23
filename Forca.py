140f20e256090171344fca0209728c724be4bec6
Matheus Henrique Da Silva <matheush5529@gmail.com>
2023-02-22 22:55:44 -0300

    Começo
    Introduzindo os primeiros comandos ao jogo

--- Forca.py
+++ Forca.py
@@ -5,7 +5,16 @@ def jogar():
     print('Jogo de Forca!')
     print('****************************')
 
+palavra_secreta =  'banana'
+
+enforcou = False
+acertou - False
+
+#enquanto não enforcou e não acertou
+while(not enforcou and not acertou):
+    print('jogando...')
 
     print('Fim do jogo')
+
 if(__name__ == "__main__"):
-    jogar()
+    jogar()  
